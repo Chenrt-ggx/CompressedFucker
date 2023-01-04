66452a91f4ca6ed525010b5a650b8a539b0a8c7a
Chenrt <2926777985@qq.com>
2023-01-04 21:18:45 +0800

    fix zip in linux

--- lib/test_unzip.py
+++ lib/test_unzip.py
@@ -1,7 +1,4 @@
 import zipfile
-import platform
-import subprocess
-from lib.pwd_loader import fix_password
 from lib.lib_exception import CompressedFileNotExist, WrongZipFileFormat
 
 
@@ -17,29 +14,13 @@ def file_check(file: str) -> None:
 
 
 def test_unzip(file: str, pwd: str = None) -> bool:
-    def win_test(w_file: str, w_pwd: str = None) -> bool:
-        import pyzipper as lib
-        try:
-            with lib.AESZipFile(w_file) as w_file:
-                w_file.setpassword(w_pwd.encode())
-                w_file.testzip()
-            return True
-        except RuntimeError as exception:
-            if 'Bad password' in str(exception):
-                return False
-            raise exception
-
-    def linux_test(l_file: str, l_pwd: str = None) -> bool:
-        cmd = ['7z', 't', l_file]
-        if l_pwd is not None:
-            cmd.append(fix_password(l_pwd))
-        cmd = ' '.join(cmd)
-        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='gbk', shell=True)
-        stdout, stderr = proc.communicate()
-        return 'Everything is Ok' in stdout and len(stderr) == 0
-
-    if 'Windows'.lower() in platform.platform().lower():
-        return win_test(file, pwd)
-    elif 'Linux'.lower() in platform.platform().lower():
-        return linux_test(file, pwd)
-    return False
+    import pyzipper as lib
+    try:
+        with lib.AESZipFile(file) as w_file:
+            w_file.setpassword(pwd.encode())
+            w_file.testzip()
+        return True
+    except RuntimeError as exception:
+        if 'Bad password' in str(exception):
+            return False
+        raise exception
